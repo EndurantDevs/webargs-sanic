1d1ce8403c6e0d8bead1751113f8b05a8798dfad
Sergey Taran <taransergey@gmail.com>
2018-09-18 12:48:52 +0300

    Update example: add webargs exception handling

--- examples/user_simple_storage/extentions/exceptions.py
+++ examples/user_simple_storage/extentions/exceptions.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from sanic import Blueprint
 from sanic.exceptions import NotFound
 from sanic.response import json
+from webargs_sanic.sanicparser import HandleValidationError
 
 from extentions.helpers import format_error
 from extentions.helpers import return_an_error
@@ -18,3 +19,9 @@ def handle_404(request, exception):
     '''
     error = format_error(title='Resource not found', detail=str(exception))
     return json(return_an_error(error), status=HTTPStatus.NOT_FOUND)
+
+
+@blueprint.exception(HandleValidationError)
+def handle_422(request, exception):
+    error = {"messages": exception.exc.messages}
+    return json(error, status=HTTPStatus.UNPROCESSABLE_ENTITY)
